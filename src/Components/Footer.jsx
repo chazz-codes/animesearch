b09637b6a59d07ca12ee4db4784c0ea138820a7c
Chazz <Chazzsplash@gmail.com>
2021-05-21 11:13:43 -0400

    more styling

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,6 +7,8 @@ class Footer extends Component {
             <div className="footer">
               <h1> Thanks for viewing my site! <br/>
                <a href='www.github.com/chazzsplash'>More</a> </h1>
+
+               <h6>Powered by GIPHY and Animechan</h6>
             </div>
             </div>
         );
--- src/Components/Greeting.jsx
+++ src/Components/Greeting.jsx
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 class Greeting extends Component {
     render() {
         return (
-            <div className="header">
-                <span id="greeting">
-                <h1>Welcome to the Anime Search App </h1>
-                </span>
+            <div className="header2">
+                <div id="greeting">
+                <h1>Welcome to the Anime Quote App </h1>
+                </div>
             </div>
         );
     }
